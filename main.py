9faa5dfe799438b14be9b1529500f5cfd60f0b30
Tinu205 <dazleroger404@gmail.com>
2024-06-09 00:56:18 +0530

    Implemented some code but ping is slower

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import socket
+from ping3 import ping, verbose_ping
+import psutil
 
-host = socket.gethostname()
-ip = socket.gethostbyname(hostname)
+ip_list=[]
 
-print(r"{host}->{ip}")
+def get_ip_address():
+    # Get the hostname
+    hostname = socket.gethostname()
+    ip_addr = socket.gethostbyname(hostname)
+    return ip_addr
+
+def ip_split():
+    ip = get_ip_address()
+    x = ip.split(".")
+    x = x[0:3]
+    return x
+
+def get_ping_response(ip):
+    response_time = ping(ip)
+    if response_time is not None:
+        ip_list.append(ip)
+
+if __name__ == "__main__":
+    print(get_ip_address())
+    ip = ip_split()
+    for i in range(1,255):
+        print(f"Entering the loop at ip{i} ")
+
+        joint_ip = f"{ip[0]}.{ip[1]}.{ip[2]}.{i}"
+        get_ping_response(joint_ip)
+    print(ip_list)
